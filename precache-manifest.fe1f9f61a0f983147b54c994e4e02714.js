243ad618b4de053242872b76851be0252a5781c1
Petro Maliar <maliar@zfort.com>
2020-03-25 15:04:38 +0200

    Updates

---
+++ precache-manifest.fe1f9f61a0f983147b54c994e4e02714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001fd70e7131ef7c852758c35423fd7f",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "fdbf6d1d3c55a11a94e2",
+    "url": "/monsters/static/css/main.fa152c54.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdbf6d1d3c55a11a94e2",
+    "url": "/monsters/static/js/main.9dd79b6a.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
